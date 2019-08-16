a17587e0c9c6ce36ad0eb73caf4bc0ef37e79997
Alexander Thiel <github@alexthiel.de>
2019-08-16 09:35:27 +0200

    feat: initial commit

---
+++ data.js
@@ -0,0 +1,99 @@
+const data = [
+    {
+        initiative: "ClientEarth",
+        bio: {
+            content: "Hallo Heinz",
+            updatedBy: "Alexander", updatedAt: "2019-08-06"
+        },
+        legalForm: {
+            content: "GmbH",
+            updatedBy: "Alexander", updatedAt: "2019-08-06"
+        },
+        established: {
+            content: "1997",
+            updatedBy: "Alexander", updatedAt: "2019-08-06"
+        },
+        // kpis: {
+        //     content: "People in custody\nParticipants in actions\nMembers\nPress Mentions",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        //
+        // },
+        // employees: {
+        //     content: "4000",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // members: {
+        //     content: "unknown",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // activeCountries: {
+        //     content: "3",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // yearlyIncome: {
+        //     content: "5000$, not entirely clear",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // socialMedia: {
+        //     content: "300 likes, 1000 followers",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // pressArticles: {
+        //     content: "none",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // lobbyistsRegistered: {
+        //     content: "none",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // billsDrafted: {
+        //     content: "unknown",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+        // billsEnacted: {
+        //     content: "4",
+        //     updatedBy: "Alexander", updatedAt: "2019-08-06",
+        // },
+
+    },
+    {
+        initiative: "Extinction Rebellion",
+        established: {
+            content: "1997",
+            updatedBy: "Alexander", updatedAt: "2019-08-06"
+        },
+    },
+    {
+        initiative: "Extinction Rebellion",
+    },
+    {
+        initiative: "WWF",
+    },
+    {
+        initiative: "Greenpeace",
+    },
+    {
+        initiative: "Ende Gelände",
+    },
+    {
+        initiative: "CERES",
+    },
+    {
+        initiative: "World Ressource Institution",
+    },
+    {
+        initiative: "350.org",
+    },
+    {
+        initiative: "Sierra Club",
+    },
+    {
+        initiative: "Idle no more",
+    },
+    {
+        initiative: "Union of concerned Scientists",
+    },
+    {
+        initiative: "Hambacher Forst",
+    },
+];
---
+++ fields.js
@@ -0,0 +1,47 @@
+const fields = {
+
+    basics: {
+        title: 'Basics',
+        children: {
+            initiative: {title: 'Initiative'},
+            bio: {title: 'Bio'},
+            legalForm: {title: 'Legal Form'},
+            established: {title: 'Established'},
+            kpis: {title: 'KPIs'},
+        }
+    },
+    people: {
+        title: 'People',
+        children: {
+            employees: {title: 'Employees'},
+            members: {title: 'Members'},
+        },
+    },
+    internationality: {
+        title: 'Internationality',
+        children: {
+            activeCountries: {title: 'Active Countries'},
+        },
+    },
+    resources: {
+        title: 'Resources',
+        children: {
+            yearlyIncome: {title: 'Yearly Income'},
+        },
+    },
+    reach: {
+        title: 'Reach',
+        children: {
+            socialMedia: {title: 'Social Media'},
+            pressArticles: {title: 'Press Articles'},
+        },
+    },
+    legislativeParticipation: {
+        title: 'Legislative Participation',
+        children: {
+            lobbyistsRegistered: {title: 'Registered Lobbyists'},
+            billsDrafted: {title: 'Bills Drafted'},
+            billsEnacted: {title: 'Bills Enacted'},
+        },
+    },
+};
