f82cf603c08740f701ade465e57292a4671c6c76
yeah800 <andre@extinctionfighters.org>
2019-08-18 17:24:47 +0200

    updated wwf

--- data.js
+++ data.js
@@ -69,36 +69,35 @@ const data = [
     {
         meta: {
             name: "WWF",            
-            link: "https://rebellion.earth",
-            label: "extinction-rebellion",
+            link: "http://wwf.panda.org/",
+            label: "WWF",
         },
         description: {
-            content: "Extinction Rebellion (XR) is an international movement that uses non-violent civil disobedience in an attempt to halt mass extinction and minimise the risk of social collapse.",
-            updatedBy: "Alexander", updatedAt: "2019-08-18"
+            content: "WWF is active since 1961 and therefore a well experienced player within the environmental engagement game. The biggest Pro on WWF is that it is well known and People trust the organization. They do not have a precise focus on the climate crises but there is also a focus on it. One of their goals for 2030 is to reduce greenhouse gas emissions by 40%.",
+            updatedBy: "Andre", updatedAt: "2019-08-18"
         },
         established: {
-            content: "2018",
-            updatedBy: "Alexander", updatedAt: "2019-08-06"
+            content: "[1961](http://wwf.panda.org/knowledge_hub/history/)",
+            updatedBy: "Andre", updatedAt: "2019-08-06"
         },
         kpis: {
-            content: "- People in custody\n- Participants in actions\n- Members\n- Press Mentions",
-            updatedBy: "Alexander", updatedAt: "2019-08-06",
+            content: "- Actions done / pa \n- Participants in actions\n- Members\n- Press Mentions",
+            updatedBy: "Andre", updatedAt: "2019-08-06",
 
         },
         activeCountries: {
-            content: "It's largest base is in the UK but local groups are forming in hundreds of locations worldwide.",
-            updatedBy: "Alexander", updatedAt: "2019-08-18",
+            content: "[about 100 countries](http://wwf.panda.org/knowledge_hub/history/50_years_of_achievements/)",
+            updatedBy: "Andre", updatedAt: "2019-08-18",
 
         },
         actions: {
-            content: "XR had it's most visible action so far when it caused [disruption in 5 UK cities in July 2019 (e.g. by blocking bridges in London)](https://rebellion.earth/2019/07/10/from-monday-15-july-extinction-rebellions-summer-uprising-act-now-to-cause-major-disruption-across-five-uk-cities/). " +
-                "Other, smaller acts of civil disobedience happen increasingly frequently world-wide.",
-            updatedBy: "Alexander", updatedAt: "2019-08-18",
+            content: "-",
+            updatedBy: "Andre", updatedAt: "2019-08-18",
 
         },
         pressArticles: {
-            content: "They have a [press section](https://rebellion.earth/press/). No structured assessment has happened yet.",
-            updatedBy: "Alexander", updatedAt: "2019-08-18",
+            content: "-",
+            updatedBy: "Andre", updatedAt: "2019-08-18",
 
         },
 
