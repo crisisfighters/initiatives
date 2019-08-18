8ca21c567054ef6b57db71b6a1d3a37c909f3797
Alexander Thiel <github@alexthiel.de>
2019-08-18 15:45:04 +0200

    adds description, countries, employees and income for ClientEarth

--- data.js
+++ data.js
@@ -3,14 +3,30 @@ const data = [
         meta: {
             name: "ClientEarth",
             link: "https://www.clientearth.org/",
+            label: "client-earth",
         },
         description: {
-            content: "Hallo Heinz",
-            updatedBy: "Alexander", updatedAt: "2019-08-06"
+            content: "ClientEarth is a charity that uses the power of the law to protect the planet and the people who live on it. We are lawyers and environmental experts who are fighting against climate change and to protect nature and the environment.",
+            updatedBy: "Alexander", updatedAt: "2019-08-18"
         },
         established: {
             content: "2008",
             updatedBy: "Alexander", updatedAt: "2019-08-06"
+        },
+        activeCountries: {
+            content: "Europe, China, Central & West Africa",
+            updatedBy: "Andre", updatedAt: "2019-08-18"
+        },
+        employees: {
+            content: "- 103 in 2017\n- 43 in 2012",
+            updatedBy: "Alexander", updatedAt: "2019-08-06"
+        },
+        yearlyIncome: {
+            content: "- 11.5M £ in 2017" +
+                "\n- 7.3M £ in 2016" +
+                "\n- 3.3M £ in 2012" +
+                "\n- 2.6M £ in 2011",
+            updatedBy: "Andre", updatedAt: "2019-08-18"
         },
 
     },
