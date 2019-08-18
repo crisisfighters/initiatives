93fbf1c86c64254be8e1690644a07411d58d9274
Alexander Thiel <github@alexthiel.de>
2019-08-18 15:14:13 +0200

    adds press content

--- data.js
+++ data.js
@@ -18,6 +18,7 @@ const data = [
         meta: {
             name: "Extinction Rebellion",
             link: "https://rebellion.earth",
+            tags: "extinction-rebellion",
         },
         description: {
             content: "Extinction Rebellion (XR) is an international movement that uses non-violent civil disobedience in an attempt to halt mass extinction and minimise the risk of social collapse.",
@@ -39,6 +40,11 @@ const data = [
         },
         actions: {
             content: "XR had it's most visible action so far when it caused disruption in 5 UK cities in July 2019 (e.g. by blocking bridges in London). Other, smaller acts of civil disobedience happen increasingly frequently world-wide.",
+            updatedBy: "Alexander", updatedAt: "2019-08-18",
+
+        },
+        pressArticles: {
+            content: "They have a [press section](https://rebellion.earth/press/). No structured assessment has happened yet.",
             updatedBy: "Alexander", updatedAt: "2019-08-18",
 
         },
