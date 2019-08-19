4b74423f2df3246a10c9a189e7e428422482a3dc
Alexander Thiel <github@alexthiel.de>
2019-08-19 21:46:14 +0200

    feat: adds package.json

--- test.js
+++ test.js
@@ -2,12 +2,13 @@
 
 const {data} = require('./data');
 const {fields} = require('./fields');
+let assertions = 0;
 const assert = (b, msg) => {
     if (!b) {
         throw new Error(msg);
     }
+    assertions++;
 };
-
 assert(data.length > 5, 'expecting at least 5 initiatives');
 assert(Object.keys(fields).length > 3, 'expecting at least 3 groups');
 
@@ -17,3 +18,5 @@ for (const row of data) {
     assert(row.meta.label, 'expecting field `meta.label` for ' + row.meta.name);
     assert(row.meta.link, 'expecting field `meta.link` for ' + row.meta.name);
 }
+
+console.log(`Looks good. ${assertions} assertions met.`);
